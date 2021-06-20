beb3c8aae6695cf964c2a168109be78079aa81a7
Eric Wang <wjun0912@gmail.com>
2021-06-20 12:52:08 +1000

    playground project to reuse esbuild config (#2)

--- playground/run-tool.js
+++ playground/run-tool.js
@@ -1,12 +1,2 @@
-const path = require('path');
-
-require('ts-node').register({
-  project: path.join(
-    __dirname,
-    '..',
-    'tsconfig.json',
-  ),
-  transpileOnly: true,
-});
-
+require('./register-tsnode');
 require('../src/index').main();
